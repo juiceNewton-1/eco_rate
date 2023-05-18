a9b17b002036852433a24310cbdc949fb136375d
overflow-1 <vladgolec121@mail.ru>
2023-05-18 16:42:22 +0300

    Add geolocator_service to the app

---
+++ lib/services/geolocator_service.dart
@@ -0,0 +1,25 @@
+import 'dart:developer';
+
+import 'package:geolocator/geolocator.dart';
+
+class GeolocatorService {
+  Future<Position?> getUserPosition() async {
+    final permission = await Geolocator.checkPermission();
+    try {
+      if (permission == LocationPermission.always ||
+          permission == LocationPermission.whileInUse) {
+        final position = await Geolocator.getCurrentPosition();
+        return position;
+      } else if (permission == LocationPermission.deniedForever ||
+          permission == LocationPermission.denied) {
+        return null;
+      } else {
+        await Geolocator.requestPermission();
+        return await getUserPosition();
+      }
+    } catch (e) {
+      log('GeolocatorService || $e');
+      rethrow;
+    }
+  }
+}
