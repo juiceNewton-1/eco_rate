cee62157a5aba8eaa22053e9f6c40725f5485d5c
overflow-1 <vladgolec121@mail.ru>
2023-05-20 18:42:48 +0300

    Add PageRouteBuilder to the onGenerateRoute value

--- lib/main.dart
+++ lib/main.dart
@@ -12,17 +12,32 @@ class MyApp extends StatelessWidget {
     super.key,
   });
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final routes = AppRoutes.getRoutes(context);
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<LogicController>(
             create: (_) => LogicController())
       ],
       child: MaterialApp(
+        theme: ThemeData(useMaterial3: true),
         debugShowCheckedModeBanner: false,
-        routes: AppRoutes.getRoutes(context),
+        routes: routes,
+        onGenerateRoute: (settings) => PageRouteBuilder(
+            settings: settings,
+            pageBuilder: (context, animation, secondaryAnimation) {
+              final RouteNaming pageContentBuilder = routes[settings.name]!;
+              return pageContentBuilder(context);
+            },
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return FadeTransition(
+                opacity: animation,
+                child: child,
+              );
+            },
+            transitionDuration: const Duration(milliseconds: 500)),
       ),
     );
   }
