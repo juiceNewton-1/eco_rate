30c4ad422520426b2f79cfa3b3c8e65341cd6d6a
overflow-1 <vladgolec121@mail.ru>
2023-05-20 18:41:04 +0300

    Add AreaType

---
+++ lib/constants/area_type.dart
@@ -0,0 +1,14 @@
+import 'package:diplom/constants/enums.dart';
+
+class AreaType {
+  static const areaTypeNames = <UserAreaType, String>{
+    UserAreaType.residential: 'Селитебная зона',
+    UserAreaType.industrial: 'Промышленная зона',
+    UserAreaType.recreation: 'Зона отдыха',
+    UserAreaType.agricultural: 'Сельскохозяйственная зона',
+    UserAreaType.reserve: "Заповедники"
+  };
+  static const areaTypeDescritpion = <UserAreaType, String>{};
+
+  
+}
