12b24faae4971a10f53db56590c6b84d6c96678a
overflow-1 <vladgolec121@mail.ru>
2023-05-20 18:40:16 +0300

    Add radial animation to the home_screen

--- lib/widgets/screens/main_screen/bottom_screens/home_screen.dart
+++ lib/widgets/screens/main_screen/bottom_screens/home_screen.dart
@@ -3,25 +3,42 @@ import 'dart:async' show StreamSubscription;
 import 'package:diplom/controllers/logic_controller.dart';
 import 'package:diplom/models/air_pollution_model.dart';
 import 'package:diplom/models/user_position_model.dart';
+import 'package:diplom/widgets/components/circle_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen>
     with SingleTickerProviderStateMixin {
   late final LogicController _logicController;
   late final AnimationController _animationController;
+  Animation<double>? _animation;
+  double _currentNoiseLevel = 0.0;
   @override
   void initState() {
-    _animationController = AnimationController(vsync: this);
     _logicController = context.read<LogicController>();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 100),
+    );
+
+    _animation = Tween(begin: 0.0, end: 120.0).animate(_animationController);
+
+    _animationController.forward();
+
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -29,46 +46,44 @@ class _HomeScreenState extends State<HomeScreen>
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        FutureBuilder(
-            future: _logicController.getCommonData(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                final userPostition =
-                    snapshot.data!['userPosition'] as UserPositionModel;
-                final aitPollutionModel =
-                    snapshot.data!['airPollutionModel'] as AirPollutionModel;
-                return Column(
-                  children: [
-                    Text(userPostition.cityName),
-                    Text(aitPollutionModel.co[0].toString())
-                  ],
-                );
-              } else if (snapshot.hasError) {
-                return const Text('Error');
-              } else {
-                return const CircularProgressIndicator();
-              }
-            }),
         StreamBuilder(
           stream: _logicController.noiseStream,
           builder: (context, snapshot) {
-            return snapshot.hasData
-                ? Container(
-                    width: 100,
+            if (snapshot.hasData) {
+              _currentNoiseLevel = snapshot.data!.meanDecibel;
+              _animationController.animateTo(_currentNoiseLevel / 120);
+              return Stack(
+                alignment: Alignment.center,
+                children: [
+                  SizedBox(
                     height: 100,
-                    color: Color.lerp(Colors.green, Colors.red,
-                        snapshot.data!.meanDecibel / 120),
-                  )
-                : const CircularProgressIndicator();
+                    width: 100,
+                    child: AnimatedBuilder(
+                      animation: _animationController,
+                      builder: (context, child) {
+                        return CustomPaint(
+                          painter: CircleIndicator(
+                            fillColor: Color.lerp(
+                              Colors.green,
+                              Colors.red,
+                              _currentNoiseLevel / 120,
+                            )!,
+                            fillValue: _currentNoiseLevel,
+                            divisionValue: 120,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  Text('${_currentNoiseLevel.ceil()} dB')
+                ],
+              );
+            } else {
+              return const CircularProgressIndicator();
+            }
           },
         ),
       ],
     );
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
   }
 }
